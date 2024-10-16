3723d6835f2960aaee322c38a02bedd06853fe03
Serpentariu13 <serpentarium13@mail.ru>
2024-10-16 13:23:57 +0300

    add vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,19 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
-import node from "@astrojs/node";
+// import node from "@astrojs/node";
 import tailwind from "@astrojs/tailwind";
+import vercelStatic from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  // output: "static",
-  output: "hybrid",
+  output: "static",
+  adapter: vercelStatic(),
 
-  adapter: node({
-    mode: "standalone",
-  }),
+  // output: "hybrid",
+
+  // adapter: node({
+  //   mode: "standalone",
+  // }),
 });
