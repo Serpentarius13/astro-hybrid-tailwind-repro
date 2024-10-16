a830c9559f0384708c182f35a6751258bd27a2e9
Serpentariu13 <serpentarium13@mail.ru>
2024-10-16 13:26:02 +0300

    Add vercel serverless

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,13 @@ import { defineConfig } from "astro/config";
 
 // import node from "@astrojs/node";
 import tailwind from "@astrojs/tailwind";
-import vercelStatic from "@astrojs/vercel/static";
+import vercelServerless from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: "static",
-  adapter: vercelStatic(),
+  output: "hybrid",
+  adapter: vercelServerless(),
 
   // output: "hybrid",
 
